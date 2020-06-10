6f3bf9a4f34d042a46de1d02717ff580178e08ec
opeyemi-jonah <gavrieljonah@gmail.com>
2020-06-10 16:23:09 -0600

    decalring vars

--- DOM_pt2/content_Interaction.js
+++ DOM_pt2/content_Interaction.js
@@ -1,1 +1,7 @@
 var p = document.querySelector('p');
+var special = document.querySelector("#special")
+var specialA = document.querySelector("a")
+
+specialA.getAttribute("href")
+
+specialA.setAttribute("href","https://www.jonah-fullstack.com")
